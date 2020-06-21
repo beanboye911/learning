8bf5cb12c507ce7022740ac4b1d1da4d4c918e95
unknown <azsage78@gmail.com>
2020-06-21 16:00:13 -0700

    thing wit spell error

---
+++ class2/equation.py
@@ -0,0 +1,7 @@
+
+print("this is a tool to solve quadratic equations. ")
+a = float(input("pls type. "))
+b = float(input("pls type second. "))
+c = float(input("pls type threed. "))
+print("your equation is %gx^2+%gx+%g=0" %(a, b, c))
+print("solution to equasions is: ")
