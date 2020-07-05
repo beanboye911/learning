961ad72c8c97037cab5b4a30bd193dcf3f151242
unknown <azsage78@gmail.com>
2020-07-05 16:02:41 -0700

    graph

--- class6/plot.py
+++ class6/plot.py
@@ -1,1 +1,14 @@
-
+import matplotlib.pyplot as plt
+xx = []
+y = []
+for i in range(200):
+    xx.append(i*0.1 - 10)
+for x in xx:
+    y.append(x*x)
+plt.plot(xx, y)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.plot(xx, y)
+plt.title('yeah boi')
+#plt.show()
+plt.savefig('plot.png')
