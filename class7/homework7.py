e3bc40c7190e8bf977cc54a6a1e0d72e6e5d94d1
unknown <azsage78@gmail.com>
2020-07-12 15:27:06 -0700

    i needed help

--- class7/homework7.py
+++ class7/homework7.py
@@ -5,6 +5,13 @@ student_dict[1] = jason
 
 amy = school.Student(id = 2, name = 'Amy', age = 15, score = 20)
 student_dict[2] = amy
-amy.print()
 
-input()
+yin = school.Student(id = 3, name = 'Yin', age = 45, score = 30)
+student_dict[3] = yin
+
+direct = int(input('what is your student id'))
+
+if direct not in student_dict:
+    print("cant find student oops")
+else:
+    student_dict.get(direct).print()
