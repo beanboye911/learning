e81914f3b100eed63551e1d4491ac037f34f8004
unknown <azsage78@gmail.com>
2020-08-01 15:47:13 -0700

    pictures. :)

--- class9/example.py
+++ class9/example.py
@@ -1,1 +1,24 @@
+from PIL import Image
+from PIL import ImageFilter
+im = Image.open('lemon.png')
 
+im.show()
+
+print(im.format, im.size, im.mode)
+size = (128, 128)
+im.thumbnail(size)
+im.show()
+im.save('thumbnail.jpg', 'JPEG')
+
+rotated = im.rotate(45)
+rotated.save('rotated.jpg', 'JPEG')
+rotated.show()
+
+transposed = im.transpose(Image.FLIP_TOP_BOTTOM)
+transposed.show()
+
+detail = im.filter(ImageFilter.DETAIL)
+detail.show()
+
+greyscale = im.convert('L')
+greyscale.show()
